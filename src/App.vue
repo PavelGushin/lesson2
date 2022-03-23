d1252c11ad29e849511cdc474020001f66b5ec15
PavelGushin <92298137+PavelGushin@users.noreply.github.com>
2022-03-23 20:59:07 +0300

    Задача 1

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,65 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="d-flex">
+      <table-component
+        :userData="tblData"
+        class="me-5 ms-5"
+        v-on:move="onMove"
+      />
+      <table-component
+        :userData="tblData2"
+        class="me-5 ms-5"
+        v-on:move="onMove"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import tableComponent from "./components/tableComponent.vue";
+import jsonData from "./tblData.json";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    tableComponent,
+  },
+  data() {
+    return {
+      tblData: jsonData,
+      tblData2: [
+        {
+          id: 3,
+          login: "Alex",
+        },
+        {
+          id: 4,
+          login: "Joe",
+        },
+      ],
+    };
+  },
+  methods: {
+    onMove: function (data) {
+      console.log(data);
+      let result = this.tblData.filter(function (item) {
+        return item.id !== data.id;
+      });
+      if (result.length === this.tblData.length) {
+        this.tblData.push(data);
+
+        result = this.tblData2.filter(function (item) {
+          return item.id !== data.id;
+        });
+
+        this.tblData2 = result;
+      } else {
+        this.tblData2.push(data);
+        this.tblData = result;
+      }
+    },
+  },
+};
 </script>
 
 <style>
---
+++ src/components/tableComponent.vue
@@ -0,0 +1,41 @@
+<template>
+  <table class="table table-dark">
+    <thead>
+      <tr>
+        <th scope="col">id</th>
+        <th scope="col">Имя пользователя</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="item in userData"
+        :key="item.id"
+        class="pointer"
+        v-on:click="send(item)"
+      >
+        <th>{{ item.id }}</th>
+        <th>{{ item.login }}</th>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "tableComponent",
+  props: ["userData"],
+  methods: {
+    send: function (item) {
+      this.$emit("move", item);
+    },
+  },
+};
+</script>
+
+
+<style scoped>
+.pointer {
+  cursor: pointer;
+}
+</style>
+
